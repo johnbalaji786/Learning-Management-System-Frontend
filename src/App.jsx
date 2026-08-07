5fc2068970330da110e3da8d63966f4486951f5f
Blaji <johnbalaji786@gmail.com>
2026-08-07 14:42:32 +0530

    Fix router configuration and add tutor payments route

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 import { ToastContainer } from "react-toastify";
+import { Provider } from "react-redux";
+
+import store from "./redux/store";
+
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
-import { Provider } from "react-redux";
-import store from "./redux/store";
 import StudentDashboard from "./pages/StudentDashboard";
 import TutorDashboard from "./pages/TutorDashboard";
 import AdminDashboard from "./pages/AdminDashboard";
-import { adminLoader, studentLoader, tutorLoader } from "./loaders/roleLoaders";
 import Courses from "./pages/Courses";
 import CourseDetails from "./pages/CourseDetails";
 import CreateCourse from "./pages/CreateCourse";
@@ -16,11 +17,12 @@ import MyCourses from "./pages/MyCourses";
 import EditCourse from "./pages/EditCourse";
 import MyBookings from "./pages/MyBookings";
 import TutorBookings from "./pages/TutorBookings";
+import TutorPayments from "./pages/TutorPayments";
 import ManageUsers from "./pages/ManageUsers";
 import Tutors from "./pages/Tutors";
-import authLoader from "./loaders/authLoader";
 import NotFound from "./pages/NotFound";
-import TutorPayments from "./pages/TutorPayments";
+
+import { adminLoader, studentLoader, tutorLoader } from "./loaders/roleLoaders";
 
 const router = createBrowserRouter([
   {
@@ -39,31 +41,16 @@ const router = createBrowserRouter([
     path: "/student/dashboard",
     element: <StudentDashboard />,
     loader: studentLoader,
-    hydrateFallbackElement: (
-      <div className="flex items-center justify-center min-h-screen">
-        <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
-      </div>
-    ),
   },
   {
     path: "/tutor/dashboard",
     element: <TutorDashboard />,
     loader: tutorLoader,
-    hydrateFallbackElement: (
-      <div className="flex items-center justify-center min-h-screen">
-        <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
-      </div>
-    ),
   },
   {
     path: "/admin/dashboard",
     element: <AdminDashboard />,
     loader: adminLoader,
-    hydrateFallbackElement: (
-      <div className="flex items-center justify-center min-h-screen">
-        <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
-      </div>
-    ),
   },
   {
     path: "/courses",
@@ -99,6 +86,11 @@ const router = createBrowserRouter([
     loader: tutorLoader,
   },
   {
+    path: "/tutor/payments",
+    element: <TutorPayments />,
+    loader: tutorLoader,
+  },
+  {
     path: "/users",
     element: <ManageUsers />,
     loader: adminLoader,
@@ -111,20 +103,19 @@ const router = createBrowserRouter([
     path: "*",
     element: <NotFound />,
   },
-  <Route path="/tutor/payments" element={<TutorPayments />} />,
 ]);
 
-const App = () => {
+function App() {
   return (
     <Provider store={store}>
       <RouterProvider router={router} />
+
       <ToastContainer
         position="top-right"
         autoClose={3000}
         hideProgressBar={false}
         newestOnTop={false}
         closeOnClick
-        rtl={false}
         pauseOnFocusLoss
         draggable
         pauseOnHover
@@ -132,6 +123,6 @@ const App = () => {
       />
     </Provider>
   );
-};
+}
 
 export default App;
